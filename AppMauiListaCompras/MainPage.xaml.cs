b9891c3be67b6e9418144e42b2876a8bf1fe2a30
Usuario <Usuario@CPD03-M07>
2024-03-14 14:58:10 -0300

    Backend listagem

--- AppMauiListaCompras/MainPage.xaml.cs
+++ AppMauiListaCompras/MainPage.xaml.cs
@@ -1,25 +1,87 @@
-﻿namespace AppMauiListaCompras
+﻿using System.Collections.ObjectModel;
+using AppMauiListaCompras.Models;
+
+namespace AppMauiListaCompras
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+         ObservableCollection<Produto> lista_produto = new ObservableCollection<Produto>();
 
         public MainPage()
         {
             InitializeComponent();
+            lst_produtos.ItemsSource = lista_produto;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void ToolbarItem_Clicked_somar(object sender, EventArgs e)
         {
-            count++;
+            double soma = lista_produto.Sum(i => (i.Preco * i.Quantidade));
+            string msg = $"O total é {soma:C}";
+            DisplayAlert("Somatória", msg, "Fechar");
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
+
+        protected override void OnAppearing()
+        {
+           if(lista_produto.Count ==0) 
+           {
+                Task.Run(async () =>
+                {
+                    List<Produto> tmp = await App.Db.GetAll();
+                    foreach (Produto p in tmp)
+                    {
+                        lista_produto.Add(p);
+                    }
+                });
+           }
+        }
+
+        private void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string q = e.NewTextValue;
+            lista_produto.Clear();
+            Task.Run(async () =>
+            {
+                List<Produto> tmp = await App.Db.Search(q);
+                foreach (Produto p in tmp)
+                {
+                    lista_produto.Add(p);
+                }
+            });
+        }
+
+        private void ref_carregando_Refreshing(object sender, EventArgs e)
+        {
+            lista_produto.Clear();
+            Task.Run(async () =>
+            {
+                List<Produto> tmp = await App.Db.GetAll();
+                foreach (Produto p in tmp)
+                {
+                    lista_produto.Add(p);
+                }
+            });
+            //ref.IsRefreshing = false;
+        }
+
+        private void ToolbarItem_Clicked_Add(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+        
+
+        
+        private void MenuItem_Clicked_remover(object sender, EventArgs e)
+        {
+
+        }
+
+        
     }
 
 }
